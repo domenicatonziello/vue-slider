25dc0e913c42cda71874d9172164c5c8d3b70b1b
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-10 16:05:49 +0100

    esercizio completo

--- script.js
+++ script.js
@@ -10,3 +10,60 @@ Bonus:
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 console.log('VUE OK', Vue);
+
+// creo app
+const app = Vue.createApp({
+    data(){
+        return{
+            currentIndex: 0,
+            pictures: [
+                {
+                  image: 'img/01.webp',
+                  title: 'Marvel\'s Spiderman Miles Morale',
+                  text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, 
+                {
+                  image: 'img/02.webp',
+                  title: 'Ratchet & Clank: Rift Apart',
+                  text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                },
+                {
+                  image: 'img/03.webp',
+                  title: 'Fortnite',
+                  text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                },
+                {
+                  image: 'img/04.webp',
+                  title: 'Stray',
+                  text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, 
+                {
+                  image: 'img/05.webp',
+                  title: "Marvel's Avengers",
+                  text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ]
+        }
+    },
+    methods:{
+        changePic(target){
+            if(target === 'down'){
+                this.currentIndex++;
+                if (this.currentIndex === this.pictures.length){
+                    this.currentIndex = 0; 
+                }
+
+            } else{
+                this.currentIndex--;
+                if (this.currentIndex < 0){
+                    this.currentIndex = this.pictures.length -1; 
+                }
+
+            }
+        }
+
+    }
+
+});
+// monto app
+app.mount('#root');
